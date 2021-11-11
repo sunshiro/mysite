8b3e4f89aae362bf0787681247818b510e76d9ab
Natthapong J <natthapong.ju@kmitl.ac.th>
2021-11-11 16:12:48 +0700

    currency

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,25 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+    return render_template('index.html')
+
+@app.route('/currency')
+def currency_form():
+    return render_template('form.html')
+
+@app.route('/result', methods = ['GET'])
+def currency_result():
     result = requests.get("http://data.fixer.io/api/latest?access_key=88e986137779229d0ead7334b6a91252")
-    data = result.json()
-    return render_template('currency.html',exchange=data)
+    jsondata = result.json()
+    eur_thb = jsondata['rates']['THB']
+    eur = request.args.get('amount')
+    thb = float(eur)*float(eur_thb)
+    return render_template('result.html',amount_eur=eur,amount_thb=thb)
 
