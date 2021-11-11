5d37f8ffb261a3f12b51f27f9aa4f0c14ee948d2
Natthapong J <natthapong.ju@kmitl.ac.th>
2021-11-11 15:33:35 +0700

    edit template

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,13 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    result = requests.get("https://hello.ddc.moph.go.th")
+    data = result.json()
+    return render_template('index.html',covid_stat=data[0])
 
